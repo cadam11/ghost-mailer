b741012d84d1e1305f1d45da903fdb4afd856e07
Craig Adam <craig@adam11.ca>
2017-05-20 17:21:27 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+var App = require('ghost-app'),
+
+    Mailer;
+
+Mailer = App.extend({
+
+    install: function () {},
+
+    uninstall: function () {},
+
+    activate: function () {},
+
+    deactivate: function () {}
+
+});
+
+module.exports = Mailer;
